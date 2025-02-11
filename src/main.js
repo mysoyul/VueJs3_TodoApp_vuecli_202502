b916ce8238f2f36a47561801c5d35f6e694e28bd
mysoyul <vega2k_@naver.com>
2025-02-11 14:00:46 +0900

    vuex store 생성

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
+import { store } from "@/store"
 
-createApp(App).mount('#app')
+createApp(App)
+//App컴포넌트가 store객체를 사용할 수 있도록 주입(injection)하기
+.use(store)
+.mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { createStore, createLogger } from "vuex";
+
+const storage = {
+  fetch() {
+    const arr = [];
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        const itemJson = localStorage.getItem(localStorage.key(i));
+        if (itemJson) {
+          arr.push(JSON.parse(itemJson));
+        }
+      }
+    }
+    return arr;
+  },
+};
+
+export const store = createStore({
+  plugins: process.env.NODE_ENV === "development" ? [createLogger()] : [],
+  //상태변수를 선언하는 state 객체
+  state: {
+    todoItems: storage.fetch()
+  },
+  //상태변수를 변경(set)하는 동기함수를 포함하는 객체
+  mutations: {},
+  //상태변수를 변경하는 비동기함수 (ajax 통신)를 포함하는 객체
+  actions: {},
+});
